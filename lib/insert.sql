98b7406065f752818237d68138df6bc7490849a0
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-19 05:12:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "M", "grizzly", "also grizzly", true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "M", "grizzly", "also grizzly", "true")
