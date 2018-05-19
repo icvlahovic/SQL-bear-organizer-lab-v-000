d9a3767d1bf86c0e100584431372cac670a330db
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-19 05:12:17 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,1 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "grizzly", "choleric", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Rowdy", 15, "M", "panda", "sanguine", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "F", "panda", "melancholic", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "M", "grizzly", "phlegmatic", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "F", "polar", "sanguine", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "M", "black", "melancholic", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "F", "grizzly", "phlegmatic", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 10, "M", "grizzly", "choleric", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "M", "grizzly", "also grizzly", TRUE)
